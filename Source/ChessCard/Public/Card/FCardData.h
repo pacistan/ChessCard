524ba7926b73c6ea7e1c0ac939d5725ca8414cea
Stan <fritschstanislas@gmail.com>
2024-12-06 00:44:48 +0100

    Set up data WIP + Tags

--- Source/ChessCard/Public/Card/FCardData.h
+++ Source/ChessCard/Public/Card/FCardData.h
@@ -1,12 +1,64 @@
 ﻿#pragma once
-
+#include "GameplayTagContainer.h"
 #include "FCardData.generated.h"
+
+UENUM()
+enum class ECardType : uint8
+{
+	Unit,
+	SpecificUnit,
+	Custom // Custom card type for special cards if needed /* not implemented Yet */
+};
 
 USTRUCT(BlueprintType)
 struct FCardData : public FTableRowBase
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FName CardName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card")
+	ECardType CardType = ECardType::Unit;
+
+	/* if the card is a specific unit, this is the name of the unit who can use this card */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card", meta = (editcondition = "CardType == ECardType::SpecificUnit"))
+	FString NameOfUnitWhoCanUseThisCard;
+
+	/* juste the name for the Ui, the actual name if the name of the Raw */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UIData")
+	FString CardName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UIData")
+	FString CardDescription;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UIData")
+	TSoftObjectPtr<UTexture2D> CardTexture;
+
+	// On Death 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (InlineEditConditionToggle))
+	bool bHasOnDeathEffect = false;
+	
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (editcondition = "bHasOnDeathEffect"))
+
+	// On Spawn 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (InlineEditConditionToggle))
+	bool bHasOnSpawnEffect = false;
+
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (editcondition = "bHasOnSpawnEffect"))
+	
+	// On Kill 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (InlineEditConditionToggle))
+	bool bHasOnKillEffect = false;
+
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (editcondition = "bHasOnKillEffect"))
+	
+	// On Move (end of deplacement)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (InlineEditConditionToggle))
+	bool bHasOnMoveEffect = false;
+
+	/* the Effect is Trigger at End of Movement */
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effects", meta = (editcondition = "bHasOnMoveEffect"))
+	
+	/* Tags the Unit need To have at Start */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tags")
+	FGameplayTagContainer Tags;
 };
+ 
---
+++ Source/ChessCard/Public/TileActor/CCTileActorBase.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CCTileActorBase.generated.h"
+
+/**
+ *  Base class for all the Actor that will be placed on the board
+ */
+UCLASS()
+class CHESSCARD_API ACCTileActorBase : public AActor
+{
+	GENERATED_BODY()
+	/* ------------------------------------------ MEMBERS -------------------------------------------*/
+public:
+	/* ----------------------------------------- FUNCTIONS -------------------------------------------*/
+	/* ------------------------------------------ OVERRIDES -------------------------------------------*/
+  
+};
