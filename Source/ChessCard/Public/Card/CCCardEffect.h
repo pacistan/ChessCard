9b98298bbcc9793233fedfecc9a80a5fd2199b76
Stanpac <fritschstanislas@gmail.com>
2024-12-06 18:52:11 +0100

    start card effect

---
+++ Source/ChessCard/Public/Card/CCCardEffect.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CCCardEffect.generated.h"
+
+/**
+ *  Base Class for Card Effect triggered by the Card
+ */
+UCLASS(Abstract, Blueprintable, EditInlineNew, CollapseCategories)
+class CHESSCARD_API UCCCardEffect : public UObject
+{
+	GENERATED_BODY()
+	/* ------------------------------------------ MEMBERS -------------------------------------------*/
+public:
+	
+	/* ------------------------------------------ FUNCTIONS -------------------------------------------*/
+	
+};
